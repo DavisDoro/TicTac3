81c44c4034ff080c9a0d199dbb6702fa4066f774
DavisDoro <deivs65@gmail.com>
2021-09-27 22:39:35 +0300

    learning objects

--- Program.cs
+++ Program.cs
@@ -5,19 +5,28 @@ namespace TicTac3
     public class Program
     {
         public static string[] fieldValue;
+        static int input = 0;
+        static string symbol;
         static void Main(string[] args)
         {
+            string inputString;
             GameField tictac = new GameField();
             do
             {
+                Console.Clear();
                 tictac.Print(); // void                           
-                tictac.MakeMove(); //void
+                
+                Console.Write("What is your next move: ");
+                inputString = Console.ReadLine();
+                myGame move = new myGame();
 
+                symbol = move.NextMoveBy();
+                if (move.Validate(inputString))
+                {
+                    input = int.Parse(inputString);
+                    move.MakeMove(input, symbol);
+                }
             } while (!Winner.WeHaveWinner());
-
-            //CheckForWinner(string[])
-            //AnounceWinner(int playerID)
-
             Console.WriteLine("Hello World!");
         }
     }
---
+++ myGame.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTac3
+{
+    public class myGame
+    {
+        public string Symbol { get; set; }
+        public int Coordinate { get; set; }
+        //public myGame(int coord)
+        //{
+        //    this.Coordinate = coord;
+        //}
+
+        public void MakeMove(int Coordinate, string Symbol)
+        {
+         GameField.fieldValue[Coordinate - 1] = Symbol;
+        }
+
+        public string NextMoveBy() // check who goes next - P1 or P2.
+        {
+            int XinField = 0;
+            int OinField = 0;
+            foreach (string item in GameField.fieldValue)
+            {
+                if (item == "X")
+                {
+                    XinField++;
+                }
+                else if (item == "O")
+                {
+                    OinField++;
+                }
+            }
+            if (XinField <= OinField)
+            {
+                return "X";
+            }
+            return "O";
+        }
+        public bool Validate(string input)
+        {
+            if (int.TryParse(input, out int inputInt))
+                {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+
+}
